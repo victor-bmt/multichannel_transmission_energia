4a53746218c531c9c557bf8ba57b6b7576d96a7f
victor-bmt <viktorvladimiro@mail.ru>
2018-05-23 22:30:30 +0200

    Add files via upload

---
+++ Multichannel_Transmission.ino
@@ -0,0 +1,17 @@
+#include "data.h"
+
+byte row_counter = 0;
+
+void setup()
+{
+  Serial.begin(115200);
+}
+
+void loop()
+{
+  Serial.write(data[row_counter++], COL_NUM);
+  if ( row_counter == ROWS_NUM )
+    row_counter = 0;
+  
+  delay(20);
+}
